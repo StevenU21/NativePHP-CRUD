e45b9c4244c17a03244c7fd4cafd19173865f858
StevenU21 <ulloadeifheltsteven@gmail.com>
2025-03-26 21:47:29 -0600

    Add new routes for forms, cards, charts, buttons, modals, and tables; implement 404 error page and modals and charts views

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,28 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('dashboard');
-});
+})->name('dashboard');
+
+Route::get('/forms', function () {
+    return view('sidebar/forms');
+})->name('forms');
+
+Route::get('/cards', function () {
+    return view('sidebar/cards');
+})->name('cards');
+
+Route::get('/charts', function () {
+    return view('sidebar/charts');
+})->name('charts');
+
+Route::get('/buttons', function () {
+    return view('sidebar/buttons');
+})->name('buttons');
+
+Route::get('/modals', function () {
+    return view('sidebar/modals');
+})->name('modals');
+
+Route::get('/tables', function () {
+    return view('sidebar/tables');
+})->name('tables');
